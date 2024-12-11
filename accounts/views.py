a741eba618740cb1a838ef2d6b74d20d19b16cd8
aaditya-adhikari09 <mr.adityaadhikari09@gmail.com>
2024-12-11 22:03:00 +0545

    validation add

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def register(request):
+    return render(request ,'register.html')
+
+def signup(request):
+    return render(request ,'signup.html')
+
+def logout(request):
+    return render(request,'signin.html')
+
+def account(request):
+    return HttpResponse('account')
