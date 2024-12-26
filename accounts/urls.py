b6cba0ac3232745edcd6d0bb1f50b43146dcc26e
aaditya-adhikari09 <mr.adityaadhikari09@gmail.com>
2024-12-26 15:13:17 +0545

    validation added

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('signup/',views.signup,name ='signup'),
     # path('signin/',views.signin,name ='signin'),
     path('register/',views.register,name ='register'),
-
+    path('login/',views.login,name ='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .forms import RegistrationForm
 from accounts.models import Account
+from django.contrib import messages ,auth
 
 # Create your views here.
 def register(request):
@@ -13,10 +14,14 @@ def register(request):
             phone_number = form.cleaned_data['phone_number']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-
-            user = Account.objects.create_user()
-        
-    
+            username = email.split('@')[0]
+            user = Account.objects.create_user(first_name=first_name,last_name=last_name,email = email,username = username ,password = password)
+            user.phone_number = phone_number
+            user.save()
+            messages.success(request, 'Registration successful')
+            return redirect('register')
+    else :
+        form = RegistrationForm()
     context = {
         'form' : form,
     }
@@ -30,3 +35,21 @@ def logout(request):
 
 def account(request):
     return HttpResponse('account')
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email, password =password)
+
+        if user is not None :
+            auth.login(request,user)
+            return redirect('home')
+        
+        else :
+            messages.error(request,'Invalid login credentials')
+
+
+
+    return render(request ,'login.html')
